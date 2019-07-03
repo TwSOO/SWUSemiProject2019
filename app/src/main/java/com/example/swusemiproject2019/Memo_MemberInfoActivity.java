b984e51f27c5471349678441e1154877d3e93af8
KimSuHyeon <sh975@naver.com>
2019-07-03 16:50:00 +0900

    불필요한 디자인 파일은 삭제하고, cherry 이미지를 추가함

---
+++ app/src/main/java/com/example/swusemiproject2019/Memo_MemberInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.swusemiproject2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Memo_MemberInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo__member_info);
+    }
+}

ba9a0365ee08c828e0dedb2ecbe2c37eeb156d81
KimSuHyeon <sh975@naver.com>
2019-07-04 11:20:58 +0900

    메모회원정보 액티비티에 있는 탭 기능 완성

--- app/src/main/java/com/example/swusemiproject2019/MainActivity.java
+++ app/src/main/java/com/example/swusemiproject2019/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.swusemiproject2019;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editId, editPwd; // 아이디입력칸, 패스워드입력칸
@@ -22,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         // 로그인버튼, 회원가입버튼 획득
         btnLogin = findViewById(R.id.btnLogin);
         btnSignup = findViewById(R.id.btnSignUp);
+
+        // 회원가입버튼 이벤트
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
--- app/src/main/java/com/example/swusemiproject2019/Memo_MemberInfoActivity.java
+++ app/src/main/java/com/example/swusemiproject2019/Memo_MemberInfoActivity.java
@@ -1,14 +1,88 @@
 package com.example.swusemiproject2019;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
 public class Memo_MemberInfoActivity extends AppCompatActivity {
+    private TabLayout tabMemoMember; // 탭영역
+    private ViewPager viewPager; // 탭별 프레그먼트 표시 영역
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo__member_info);
+
+        // 탭, 뷰페이저 획득
+        tabMemoMember = findViewById(R.id.tabMemoMember);
+        viewPager = findViewById(R.id.viewPager);
+
+        // Tab 생성
+        tabMemoMember.addTab(tabMemoMember.newTab().setText("메모"));
+        tabMemoMember.addTab(tabMemoMember.newTab().setText("회원정보"));
+        tabMemoMember.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // 어댑터 생성
+        MemoMemberAdapter adapter = new MemoMemberAdapter(getSupportFragmentManager(), tabMemoMember.getTabCount());
+
+        //뷰페이저에 어댑터 add
+        viewPager.setAdapter(adapter);
+
+        //뷰페이저에 리스너 등록
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabMemoMember));
+        //TabLayout에 리스너 등록
+        tabMemoMember.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) { // 특정 Tab 선택 이벤트
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
+    // 어댑터 클래스
+    private class MemoMemberAdapter extends FragmentPagerAdapter {
+        int tabSize; // Tab 수
+
+        // constructor
+        public MemoMemberAdapter(FragmentManager fm, int count) {
+            super(fm);
+            this.tabSize = count; // tab 수 설정
+        }
+
+        // 프레그먼트 생성 및 반환
+        @Override
+        public Fragment getItem(int position) {
+
+            switch(position){
+                case 0:
+                    return new MemoListFragment();
+                case 1:
+                    return new MemberInfoFragment();
+
+            }
+            return null;
+        }
+
+        // Tab 수 반환
+        @Override
+        public int getCount() {
+            return this.tabSize;
+        }
+    } // class
 }
